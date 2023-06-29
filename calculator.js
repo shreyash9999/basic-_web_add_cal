06ca9e94f4403c93da0771de7a9692bba16ec2f9
shreyash9999 <dongareshreyash20@gmail.com>
2023-06-29 16:37:59 +0530

    add_file_cal

--- calculator.js
+++ calculator.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
+                                //imp to declar //allows us for nesting
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
-    res.send("HEllo");
+    res.sendFile(__dirname + "/index.html");//will find the path may it be any where + /name of file
+});
+
+app.post("/",function(req, res){
+    
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1+num2;
+
+    res.send("result of calulaton is :"+ result);
 });
 
 app.listen(3000, function(){
